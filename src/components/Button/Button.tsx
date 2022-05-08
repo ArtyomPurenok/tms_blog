e3e905cf68d0f04b73328203245704c4242f8c5a
Artyom <artempurya@gmail.com>
2022-05-08 18:51:41 +0300

    correct button, add user icon

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,15 +5,16 @@ type buttonProps = {
     FunctionLikeNumber?: any
     BtnIcon?: any
     likeNumber?: number
+    BtnTxt?: string
 }
 
-export const Button = (({BtnClass, FunctionLikeNumber, BtnIcon, likeNumber}: buttonProps) => {
+export const Button = (({BtnClass, FunctionLikeNumber, BtnIcon, likeNumber, BtnTxt}: buttonProps) => {
     return <div>
         <button 
         onClick={FunctionLikeNumber}
         className={BtnClass}
         >
-
+            {BtnTxt}
             {BtnIcon && <BtnIcon height='100%'/>}
             {likeNumber}
         </button>
