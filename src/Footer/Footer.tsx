1cacea940b38c25eaa37d6587217a396e3b4924a
Artyom <artempurya@gmail.com>
2022-05-14 00:33:53 +0300

    feat: add footer and header

---
+++ src/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+
+export const Footer =(() => {
+    return <div className='footer'>
+        <hr className='footer-Hr'/>
+
+        <div className='footer-Divs-Txt'>
+            <div><p className='footer-Txt'>©2022 Blogfolio</p></div>
+            <div><p className='footer-Txt'>All rights reserved</p></div>
+        </div>
+    </div>
+})
