669c95c14e47dea3e6949045900e6c0ae28bda40
Artyom <artempurya@gmail.com>
2022-05-19 17:12:51 +0300

    correct: input.tsx

--- src/Pages/SignIn/SignInCenterDiv/SignInInputBox/SignInInputBox.tsx
+++ src/Pages/SignIn/SignInCenterDiv/SignInInputBox/SignInInputBox.tsx
@@ -5,8 +5,8 @@ import {Input} from '../../../../components/Input';
 
 export const SignInInputBox = () => {
     return <div className="signIn-input-box">
-         <Input inputName='Email' inputPlaceholder='Your email' inputClass='signIn-input-box__input-email' inputGeneralDiv='signIn-input-box__email-div'/>
-         <Input inputType='password' inputName='Password' inputPlaceholder='Your password' inputClass='signIn-input-box__input-password' inputGeneralDiv='signIn-input-box__password-div'/>
+         <Input inputName='Email' placeholder='Your email' className='signIn-input-box__input-email' generalDiv='signIn-input-box__email-div'/>
+         <Input type='password' inputName='Password' placeholder='Your password' className='signIn-input-box__input-password' generalDiv='signIn-input-box__password-div'/>
          <p className="signIn-input-box__forPas">Forgot password?</p>
     </div>
 }
